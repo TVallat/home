2423fb6eff92c453ead790d06b3ec4093347f3b7
Tval <vallat.thomas.pro@gmail.com>
2024-10-03 21:33:11 +0200

    Ajout footer et début homepage

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,55 @@
 import "./Footer.scss";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+
 
 const Footer = () => {
+
+    const location = useLocation();
+
+    useEffect(() => {
+        const navLinks = document.querySelectorAll('.foot-nav-link');
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+
+        switch (location.pathname) {
+            case '/':
+                navLinks[0].classList.add('active');
+                break;
+            case '/about':
+                navLinks[1].classList.add('active');
+                break;
+            case '/portfolio':
+                navLinks[2].classList.add('active');
+                break;
+            case '/services':
+                navLinks[3].classList.add('active');
+                break;
+            case '/resume':
+                navLinks[4].classList.add('active');
+                break;
+            case '/contact':
+                navLinks[5].classList.add('active');
+                break;
+            default: break;
+        }
+    }, [location]);
+
     return (
         <footer>
-            <div>
-                <p>Footer</p>
+            <nav>
+                <Link className="foot-nav-link" to="/">_accueil</Link>
+                <Link className="foot-nav-link" to="/about">_a_propos</Link>
+                <Link className="foot-nav-link" to="/portfolio">_portfolio</Link>
+                <Link className="foot-nav-link" to="/services">_services</Link>
+                <Link className="foot-nav-link" to="/resume">_experience</Link>
+                <Link className="foot-nav-link" to="/contact">_contact</Link>
+            </nav>
+            <div className="infos-container">
+
             </div>
         </footer>
     );
--- src/Components/Projets/Home/Home.jsx
+++ src/Components/Projets/Home/Home.jsx
@@ -1,12 +1,36 @@
 import "./Home.scss";
+import bannerImg from '../../../Images/les-cevennes.webp'
 
 const Home = () => {
     return (
-        <section>
-            <div>
-                <p>Home</p>
-            </div>
-        </section>    
+        <div>
+            <section>
+                <div>
+                    <p>// Bonjour, je suis Thomas, un...</p>
+                    <h1>{'{'}" Developpeur web "{'}'}</h1>
+                </div>
+                    <div className="banner-container">
+                        <img className="banner-image" src={bannerImg} alt="Bannière"></img>
+                    </div>
+            </section>
+            <section>
+                <div className="upper-infos">
+                    <p className="purple-text medium-size"> {'{'}/{'}'}</p>
+                    <h2>{'<'}about_me{'>'}</h2>
+                    <p className="yellow-text">...recemment diplome de la formation developpeur web chez Openclassroom</p>
+                    <p className="yellow-text">je suis a la recherche d'un premier emploi</p>
+                    <p className="yellow-text">dans le secteur</p>
+                </div>
+                <div className="lower-infos">
+                    <div className="left-infos">
+                        <p>Gauche</p>
+                    </div>
+                    <div className="right-infos">
+                        <p>Droite</p>
+                    </div>
+                </div>
+            </section>
+        </div>
     );
 }
 
